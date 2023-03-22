736169fd69bcc26dfaa598dba9f84e1659e50d60
Porhouy Minh (PH) <98186450+minhx010@users.noreply.github.com>
2023-03-22 11:52:32 -0500

    added comments

--- Friday/LiF-RETIS/basin-ext-config.sh
+++ Friday/LiF-RETIS/basin-ext-config.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 # this script is to extract a configuration right before the lambda 0 interface so that we can use it as a shooting point for the zero minus path 
 
+# find the right time/frame to dump out
+
 interface_num="basin"
 op_num=1.9 # op for defining the basin boundary 
 exttime=`awk -v op_num="$op_num" '{ if ( $2 > op_num ) {print $1} }' init-path.txt | tail -n -1` 
@@ -11,6 +13,7 @@ bop_num=`awk -v beginLNR="$beginLNR" '{ if ( NR == beginLNR ) {print $2; exit} }
 #echo $op_num $exttime $beginLNR $begintime $bop_num > test.txt
 
 # because our trajectory is reversed, the -b -e inputs would need to be flipped
+# extract the configs
 gmx trjconv -f /ocean/projects/see220002p/minhx010/LiF-RETIS/init-path.trr -o config_extraction/gl-initf_ens-${interface_num}.trr -b "$exttime" -e "$begintime"
 
 awk -v interface_num="$interface_num" -v begintime="$begintime" -v bop_num="$bop_num" 'BEGIN{print "gl-initf_ens-" interface_num " " begintime " " bop_num " " 1}' >> config_extraction/path_gl-initf_ens-${interface_num}.txt
