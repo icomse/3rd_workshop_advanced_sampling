8e275cdc07ae18ed7b56d5d4fc28da9d5e620ee8
Porhouy Minh (PH) <98186450+minhx010@users.noreply.github.com>
2023-03-22 10:43:34 -0500

    Update intf-ext-config.sh

--- Friday/LiF-RETIS/intf-ext-config.sh
+++ Friday/LiF-RETIS/intf-ext-config.sh
@@ -10,7 +10,7 @@ do
     op_num=`awk '{print $2}' <<< "$line"`
     exttime=`awk -v op_num="$op_num" '{ if ( $2 > op_num ) {print $1} }' init-path.txt | tail -n -1`
 
-    gmx trjconv -f init-path.trr -o config_extraction/gl-initf_ens-${interface_num}.trr -dump "$exttime" <<< "0" 
+    gmx trjconv -f /ocean/projects/see220002p/minhx010/LiF-RETIS/init-path.trr -o config_extraction/gl-initf_ens-${interface_num}.trr -dump "$exttime" <<< "0" 
     
     awk -v interface_num="$interface_num" -v exttime="$exttime" -v op_num="$op_num" 'BEGIN{print "gl-initf_ens-" interface_num " " exttime " " op_num " " 1}' >> config_extraction/path_gl-initf_ens-${interface_num}.txt <<< "$line"
 
